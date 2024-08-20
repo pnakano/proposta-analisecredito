00266e531509bd9718734a4a250474276df5a0e8
Pamela Nakano <pamela.nakano@hotmail.com>
2024-08-20 10:27:16 -0300

    init

---
+++ src/main/java/com/pamelanakano/proposta_analisecredito/PropostaAnalisecreditoApplication.java
@@ -0,0 +1,13 @@
+package com.pamelanakano.proposta_analisecredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaAnalisecreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaAnalisecreditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pamelanakano/proposta_analisecredito/PropostaAnalisecreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pamelanakano.proposta_analisecredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaAnalisecreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
